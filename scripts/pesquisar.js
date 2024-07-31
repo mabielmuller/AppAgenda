402e54e9cba16f749fa23cc767edab68a74f9f1a
Mabiel Muller <mabielmuller9@gmail.com>
2024-07-30 21:51:46 -0300

    Update pesquisar.js

--- scripts/pesquisar.js
+++ scripts/pesquisar.js
@@ -1,10 +1,18 @@
 const btn_pesq = document.getElementById("btn_pesq")
 const f_txtpesq = document.getElementById("f_txtpesq")
 
-btn_pesq.addEventListener("click",()=>{
-    if(f_txtpesq.value == ""){
+btn_pesq.addEventListener("click",(evt)=>{
+    const valorpesq =  f_txtpesq.value
+    if(valorpesq == ""){
         alert("Digite a pesquisa")
         f_txtpesq.focus()
         return
     }
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value
+    const endpoint = `http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorpesq}`
+    fetch(endpoint)
+    .then(res=> res.json())
+    .then(res =>{
+        console.log(res);
+    })
 })
